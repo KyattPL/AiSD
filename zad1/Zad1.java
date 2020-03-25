856aea5c61cd427b74222f67c95a3d223c3fbaae
Kajetan Pynka <siema1234@gazeta.pl>
2020-03-25 23:05:12 +0100

    Refactored code

--- zad1/Zad1.java
+++ zad1/Zad1.java
@@ -12,21 +12,20 @@ class Zad1 {
     // Oryginalna lista wczytywana z pliku .csv
     public static ArrayList<Integer> lista;
 
-    //Na potrzebę porównania różnych algorytmów sortowania:
+    // Na potrzebę porównania różnych algorytmów sortowania:
     public static int liczbaPorownan = 0;
     public static int liczbaZamian = 0;
 
     // Główna pętla programu
     public static void main(String[] args) {
-        //Tutaj z poziomu kodu podaje plik do otworzenia
-        String nazwaPliku = "z2data11.csv";
+        // Tutaj z poziomu kodu podaje plik do otworzenia
+        String nazwaPliku = "z2data12.csv";
         String pelnaSciezka = "zad1/data/" + nazwaPliku;
         wczytajDane(pelnaSciezka);
-        //Należy ustawić na cocktailSort() lub Zad2.combSort() by wybrać algorytm
-        ArrayList<Integer> posortowanaLista = Zad2.combSort();
-        //W przypadku cocktailSort'a należy zmienić "comb sort" na "cocktail sort"
-        //oraz Zad2.liczbaPorownan na liczbaPorownan oraz Zad2.liczbaZamian na liczbaZamian
-        Zad2.zapiszWyniki(nazwaPliku, posortowanaLista, "comb sort", Zad2.liczbaPorownan, Zad2.liczbaZamian);
+        // Należy ustawić na cocktailSort() lub combSort() by wybrać algorytm
+        ArrayList<Integer> posortowanaLista = combSort();
+        // W przypadku cocktailSort'a należy zmienić "comb sort" na "cocktail sort"
+        zapiszWyniki(nazwaPliku, posortowanaLista, "comb sort", liczbaPorownan, liczbaZamian);
     }
 
     // Funkcja służąca do wczytania liczb z pliku .csv. Przyjmowany argument jest
@@ -58,8 +57,8 @@ class Zad1 {
      * "odbija się" w jedną i drugą stronę.
      */
     public static ArrayList<Integer> cocktailSort() {
-        //temp od angielskiego temporary, ponieważ lista ta istnieje tylko na
-        //potrzeby tej funkcji tj. na potrzebę sortowania
+        // temp od angielskiego temporary, ponieważ lista ta istnieje tylko na
+        // potrzeby tej funkcji tj. na potrzebę sortowania
         ArrayList<Integer> tempList = (ArrayList<Integer>) lista.clone();
 
         int przesuwacz = 0;
@@ -96,7 +95,45 @@ class Zad1 {
             } else {
                 przesuwacz += 1;
             }
-            
+
+            if (czyKoniec) {
+                break;
+            }
+        }
+        return tempList;
+    }
+
+    /*
+     * Algorytm sortujący comb sort, działa jak bubble sort z 2 różnicami: -
+     * porównywane są elementy "oddalone" od siebie o pewną odległość, która z każdą
+     * kolejną iteracją się zmniejsza (ostatnia iteracja to klasyczy bubble sort) -
+     * nie mamy pewności, że po 1 iteracji ostatni element będzie maksymalnym
+     * elementem listy, dlatego zakres po którym iterujemy się nie zmniejsza
+     */
+    public static ArrayList<Integer> combSort() {
+        ArrayList<Integer> tempList = (ArrayList<Integer>) Zad1.lista.clone();
+        // Specjalnie dobrany (empirycznie) wsp. zapewnia najlepszą wydajność
+        double wspZmniejszania = 1.3;
+        int odleglosc = tempList.size();
+        boolean czyKoniec = false;
+        for (int i = 0; i <= tempList.size() - 1; i++) {
+            odleglosc = (int) Math.floor(odleglosc / wspZmniejszania);
+
+            if (odleglosc <= 1) {
+                odleglosc = 1;
+                czyKoniec = true;
+            }
+
+            for (int j = 0; j <= tempList.size() - 1 - odleglosc; j++) {
+                liczbaPorownan += 1;
+                if (tempList.get(j) > tempList.get(j + odleglosc)) {
+                    int tempVal = tempList.get(j);
+                    tempList.set(j, tempList.get(j + odleglosc));
+                    tempList.set(j + odleglosc, tempVal);
+                    liczbaZamian += 1;
+                    czyKoniec = false;
+                }
+            }
             if (czyKoniec) {
                 break;
             }
@@ -172,6 +209,19 @@ class Zad1 {
             bw.newLine();
             bw.write("Posortowana tablica: \n");
             bw.write(wydrukZbioru(posortowanaLista));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Funkcja zapisująca do pliku 'wyniki2.txt'. Zapisywana jest pojedyncza
+    // linia, która wchodzi w skład pliku 'zestawienie.txt'.
+    public static void zapiszWyniki(String nazwaPliku, ArrayList<Integer> posortowanaLista, String metodaSortowania,
+            int liczbaPor, int liczbaZam) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("zad1/wyniki2.txt"))) {
+            bw.write(nazwaPliku + " - metoda: " + metodaSortowania + ", liczebnosc: "
+                    + Zad1.liczebnoscZbioru(posortowanaLista) + ", liczba porownan: " + liczbaPor + ", liczba zamian: "
+                    + liczbaZam);
         } catch (IOException e) {
             e.printStackTrace();
         }
